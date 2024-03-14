7b30583271017716040d419b9ede46788496357a
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-14 19:34:39 -0300

    Java - MySQL connection

---
+++ src/main/java/com/example/jdbc_db_project2/JdbcDbProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_db_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDbProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDbProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdbc_db_project2/JdbcDbProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_db_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDbProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
