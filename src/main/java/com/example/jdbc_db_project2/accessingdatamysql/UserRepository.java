362daed82a1b96f1ccd4f5f186cdb2aa0e77686d
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-14 19:40:24 -0300

    Repositório configurado

---
+++ src/main/java/com/example/jdbc_db_project2/accessingdatamysql/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.jdbc_db_project2.accessingdatamysql;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.jdbc_db_project2.accessingdatamysql.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
