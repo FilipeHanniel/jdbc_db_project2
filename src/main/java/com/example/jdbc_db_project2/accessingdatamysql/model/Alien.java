a7b54d5728ed3112beae18b0cce39a57472a64af
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-15 20:00:54 -0300

    Creating the Repo Folder

---
+++ src/main/java/com/example/jdbc_db_project2/accessingdatamysql/model/Alien.java
@@ -0,0 +1,32 @@
+package com.example.jdbc_db_project2.accessingdatamysql.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Alien {
+    
+    private int id;
+    private String name;
+    private String tech;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getTech() {
+        return tech;
+    }
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+}
