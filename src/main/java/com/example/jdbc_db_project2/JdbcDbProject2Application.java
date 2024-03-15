1778c7f2f29106feca936dd5213e58b1e949de5b
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-15 20:45:10 -0300

    Creating Repo

--- src/main/java/com/example/jdbc_db_project2/JdbcDbProject2Application.java
+++ src/main/java/com/example/jdbc_db_project2/JdbcDbProject2Application.java
@@ -2,12 +2,20 @@ package com.example.jdbc_db_project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.jdbc_db_project2.accessingdatamysql.model.Alien;
 
 @SpringBootApplication
 public class JdbcDbProject2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JdbcDbProject2Application.class, args);
+		ApplicationContext context = SpringApplication.run(JdbcDbProject2Application.class, args);
+
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("Navin");
+		alien1.setTech("Java");
 	}
 
 }
---
+++ src/main/java/com/example/jdbc_db_project2/accessingdatamysql/repo/AlienRepo.java
@@ -0,0 +1,9 @@
+package com.example.jdbc_db_project2.accessingdatamysql.repo;
+
+public class AlienRepo {
+    
+    public void save(Alien alien){
+        
+    }
+
+}
