f918c3c4c3df7a1cdd10571fc7ff0ab7d6c2534d
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-16 14:17:51 -0300

    Projeto finalizado com falhas

--- src/main/java/com/example/jdbc_db_project2/JdbcDbProject2Application.java
+++ src/main/java/com/example/jdbc_db_project2/JdbcDbProject2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.example.jdbc_db_project2.accessingdatamysql.model.Alien;
+import com.example.jdbc_db_project2.accessingdatamysql.repo.AlienRepo;
 
 @SpringBootApplication
 public class JdbcDbProject2Application {
@@ -16,6 +17,11 @@ public class JdbcDbProject2Application {
 		alien1.setId(111);
 		alien1.setName("Navin");
 		alien1.setTech("Java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class);
+		repo.save(alien1);
+
+		System.out.println(repo.findAll());
 	}
 
 }
--- src/main/java/com/example/jdbc_db_project2/accessingdatamysql/repo/AlienRepo.java
+++ src/main/java/com/example/jdbc_db_project2/accessingdatamysql/repo/AlienRepo.java
@@ -1,9 +1,22 @@
 package com.example.jdbc_db_project2.accessingdatamysql.repo;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.jdbc_db_project2.accessingdatamysql.model.Alien;
+
+@Repository
 public class AlienRepo {
     
     public void save(Alien alien){
-        
+        System.out.println("added");
+    }
+
+    public List<Alien> findAll(){
+        return new ArrayList<Alien>();
     }
 
 }
